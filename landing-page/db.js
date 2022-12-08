5ef4da5ca616dc2a254889066ecbd5ee7ef09df5
MrBista <gustibisman2308@gmail.com>
2022-12-08 13:48:32 +0700

    adding landing page

---
+++ landing-page/db.js
@@ -0,0 +1,225 @@
+const dbSearch = [
+  {
+    nama: 'Bulbasur',
+    element: ['Daun', 'Racun'],
+    gambar: './assetPokemon/001.svg',
+  },
+  {
+    nama: 'Ivysaur',
+    element: ['Daun', 'Racun'],
+    gambar: './assetPokemon/002.svg',
+  },
+  {
+    nama: 'Venusaur',
+    element: ['Daun', 'Racun'],
+    gambar: './assetPokemon/003.svg',
+  },
+  {
+    nama: 'Charmender',
+    element: ['Fire'],
+    gambar: './assetPokemon/004.svg',
+  },
+  {
+    nama: 'Charmeleon',
+    element: ['Fire'],
+    gambar: './assetPokemon/005.svg',
+  },
+  {
+    nama: 'Charizard',
+    element: ['Fire'],
+    gambar: './assetPokemon/006.svg',
+  },
+  {
+    nama: 'Squirtel',
+    element: ['Water'],
+    gambar: './assetPokemon/007.svg',
+  },
+  {
+    nama: 'Wartortle',
+    element: ['Water'],
+    gambar: './assetPokemon/008.svg',
+  },
+  {
+    nama: 'Blastoise',
+    element: ['Water'],
+    gambar: './assetPokemon/009.svg',
+  },
+  {
+    nama: 'Mega Absol',
+    element: ['Dark'],
+    gambar: './asset/359.svg',
+  },
+  {
+    nama: 'Regigigas',
+    element: ['Normal'],
+    gambar: './asset/489.svg',
+  },
+
+  {
+    nama: 'Glaceon',
+    element: ['Ice'],
+    gambar: './asset/471.svg',
+  },
+
+  {
+    nama: 'Moltress',
+    element: ['Fire'],
+    gambar: './asset/146.svg',
+  },
+
+  {
+    nama: 'Articuno',
+    element: ['Ice'],
+    gambar: './asset/144.svg',
+  },
+  {
+    nama: 'Zapdos',
+    element: ['Electric'],
+    gambar: './asset/145.svg',
+  },
+
+  {
+    nama: 'Lugia',
+    element: ['Flying', 'Pyschic'],
+    gambar: './asset/249.svg',
+  },
+
+  {
+    nama: 'Primal Reversion Groundon',
+    element: ['Ground', 'Fire'],
+    gambar: './asset/383.svg',
+  },
+
+  {
+    nama: 'Primal Reversion Kyogre',
+    element: ['Water'],
+    gambar: './asset/382.svg',
+  },
+
+  {
+    nama: 'Mewtwo X',
+    element: ['Psychic', 'Fighiting'],
+    gambar: './asset/150_x.svg',
+  },
+
+  {
+    nama: 'Eternatus',
+    element: ['Poison', 'Dragon'],
+    gambar: './asset/890.svg',
+  },
+
+  {
+    nama: 'Zacian',
+    element: ['Fairy', 'Steel'],
+    gambar: './asset/888.svg',
+  },
+
+  {
+    nama: 'Zamazenta',
+    element: ['Steel'],
+    gambar: './asset/889.svg',
+  },
+
+  {
+    nama: 'Resolute  Form',
+    element: ['Water', 'Fighting'],
+    gambar: './asset/647.svg',
+  },
+  {
+    nama: 'Quaxly',
+    element: ['Water'],
+    gambar: './asset/quaxly.png',
+  },
+  {
+    nama: 'Fuecoco',
+    element: ['fire'],
+    gambar: './asset/fuecoco.png',
+  },
+  {
+    nama: 'Sprigatito',
+    element: ['grass'],
+    gambar: './asset/sprigatito.png',
+  },
+];
+
+const db_home = [
+  {
+    nama: 'Resolute  Form',
+    element: ['Water', 'Fighting'],
+    gambar: './asset/647.svg',
+  },
+  {
+    nama: 'Mega Absol',
+    element: ['Dark'],
+    gambar: './asset/359.svg',
+  },
+  {
+    nama: 'Regigigas',
+    element: ['Normal'],
+    gambar: './asset/489.svg',
+  },
+
+  {
+    nama: 'Glaceon',
+    element: ['Ice'],
+    gambar: './asset/471.svg',
+  },
+
+  {
+    nama: 'Moltress',
+    element: ['Fire'],
+    gambar: './asset/146.svg',
+  },
+  {
+    nama: 'Articuno',
+    element: ['Ice'],
+    gambar: './asset/144.svg',
+  },
+  {
+    nama: 'Mewtwo X',
+    element: ['Psychic', 'Fighiting'],
+    gambar: './asset/150_x.svg',
+  },
+
+  {
+    nama: 'Zapdos',
+    element: ['Electric'],
+    gambar: './asset/145.svg',
+  },
+
+  {
+    nama: 'Lugia',
+    element: ['Flying', 'Pyschic'],
+    gambar: './asset/249.svg',
+  },
+
+  {
+    nama: 'Primal Reversion Groundon',
+    element: ['Ground', 'Fire'],
+    gambar: './asset/383.svg',
+  },
+
+  {
+    nama: 'Primal Reversion Kyogre',
+    element: ['Water'],
+    gambar: './asset/382.svg',
+  },
+
+  {
+    nama: 'Eternatus',
+    element: ['Poison', 'Dragon'],
+    gambar: './asset/890.svg',
+  },
+
+  {
+    nama: 'Zacian',
+    element: ['Fairy', 'Steel'],
+    gambar: './asset/888.svg',
+  },
+
+  {
+    nama: 'Zamazenta',
+    element: ['Steel'],
+    gambar: './asset/889.svg',
+  },
+];
